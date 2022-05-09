d89cc4e3563bf1b5339847839e2d4c6f042ab8be
niwase.hiroaki <niwaseh@gmail.com>
2022-05-09 19:42:22 +0900

    add: log

--- src/main/java/jp/sus/web/controller/UrlShortenerWebController.java
+++ src/main/java/jp/sus/web/controller/UrlShortenerWebController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 import jp.sus.web.service.UrlShortenerWebService;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * UrlShortenerWebController.
  */
+@Slf4j
 @Controller
 public class UrlShortenerWebController {
   /** Service. */
@@ -25,10 +27,13 @@ public class UrlShortenerWebController {
    */
   @GetMapping("/{id}")
   public ModelAndView redirect(@PathVariable("id") String id) {
+    log.info("id = " + id);
     String originalUrl = service.getDestenation(id);
     if (null == originalUrl) {
+      log.info("NOT FOUND !!");
       return new ModelAndView("error/404", HttpStatus.NOT_FOUND);
     }
+    log.info("FOUND");
     return new ModelAndView("redirect:" + originalUrl);
   }
 }
